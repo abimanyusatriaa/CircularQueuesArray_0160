74103d84a4b682fc1ac51a9c7bb1053b984b6404
abimanyusatriaa <adji3890@gmail.com>
2024-05-30 10:26:20 +0700

    Add project files.

---
+++ Queue.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+class Queues {
+    int FRONT, REAR, mx = 5;
+    int queue_array[5];
+
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
