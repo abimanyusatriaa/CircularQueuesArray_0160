75f2330699dd0f1ef16a6fec4733a3bddf13cc74
abimanyusatriaa <adji3890@gmail.com>
2024-05-30 10:44:14 +0700

    else

--- Queue.cpp
+++ Queue.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class Queues {
-    int FRONT, REAR, mx = 5;
+    int FRONT, REAR, max = 5;
     int queue_array[5];
 
 public:
@@ -19,17 +19,24 @@ public:
         cout << endl;
 
         //cek apakah antrian penuh
-        if ((FRONT == 0 && max - 1) || (FRONT == REAR + 1)) {
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
             cout << "\nQueue overflow\n";
             return;
         }
 
         //cek apakah antrian kosong
         if (FRONT == -1) {
-            FRONG = 0;
+            FRONT = 0;
             REAR = 0;
         }
-
+        else {
+            //jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
 };
 
